1ba3f3162daaa1e90453218a4b6ce531c4d40f87
tmddk9445 <tmddk9445@naver.com>
2022-12-29 12:48:55 +0900

    2022-12-28 수요일 <수업>
    [웹 개발]웹프론트 - VSCode
    
    HTML
    
    javascript 기초

---
+++ javascript/js/common.js
@@ -0,0 +1,11 @@
+const helloworld = () => {
+    alert('Helloworld');
+}
+
+const moveExample02 = () => {
+    location.replace('./example02.html');
+}
+
+const back = () => {
+    history.back();
+}
